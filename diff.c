3a15afb55d0e6d8f50e134430602efe61dff85e7
Dipak Bharade <dipbharade2021@gmail.com>
2022-12-16 19:23:03 +0530

    adding diff.c

---
+++ diff.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a=0;
+    int b=0;
+    int c=0;
+
+    printf("Enter the 1st number:");
+    scanf("%d",&a);
+
+    printf("Enter the 2st number:");
+    scanf("%d",&b);
+
+    c = a-b;
+    printf("Sum= %d\n",c);
+    return 0;
+    
+}
