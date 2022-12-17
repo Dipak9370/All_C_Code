f8939f875c78845b7d2e29464c6a73fdc03b8d0b
Dipak Bharade <dipbharade2021@gmail.com>
2022-12-18 00:11:19 +0530

    Add two number using fun

--- sum.c
+++ sum.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+
+int Addition(int a,int b)
+    {
+        int c;
+        c=a+b;
+        return c;
+    }
+
 int main()
 {
     int a=0;
     int b=0;
-   // int c=0;
+    int c=0;
 
     printf("Enter the 1st number:");
     scanf("%d",&a);
@@ -11,7 +19,7 @@ int main()
     printf("Enter the 2st number:");
     scanf("%d",&b);
 
-    int c = a+b;
+    c=Addition(a,b);
     printf("Sum= %d\n",c);
     return 0;
     
